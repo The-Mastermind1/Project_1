604feecef7278137d7eed486d2e3f7029d806c69
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-11-07 00:03:22 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -38,7 +38,8 @@ int main() {
 	} while (std::fabs(fungf(x1)) > tol);
 	cout << "root found:" << x1 << '\n';
 	iterations = 0;
-	x0 = 1.5;
+	cout<<"enter again your initial guess"<<'\n'; 
+	std::cin>>x0;
 	cout << "metodos newton\n";
 	do {
 
